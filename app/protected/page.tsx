2ea5cac9e8ef25bf90bbbc3576143a053500c36d
codewithsomesh <codewithsomesh@gmail.com>
2025-10-01 23:44:36 +0800

    Fixed some deployment issues in ./app/protected/page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -35,7 +35,7 @@ export default async function ProtectedPage() {
       <div className="flex flex-col gap-2 items-start">
         <h2 className="font-bold text-2xl mb-4">Your user details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(data.claims, null, 2)}
+          {JSON.stringify(data?.claims, null, 2)}
         </pre>
       </div>
       <div>
